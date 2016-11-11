a3ef2736f6634508d77933476d81478f6160672d
Dotun Olusoga <adedotun.olusoga@gmail.com>
2016-11-10 18:38:11 -0600

    Edit Gemfile and update main view

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -1,8 +1,11 @@
 var Main = React.createClass({
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Hello World!</h1>
+        <div className="box one"></div>
+        <div className="box two"></div>
+        <div className="box three"></div>
       </div>
     )
   }
